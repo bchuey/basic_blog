bf5cd8cc305b5f6b190b3319d56becb48a22d9ec
Chris Huey <bchuey415@gmail.com>
2017-04-17 15:21:32 -0700

    basic user permission

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from urllib import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
@@ -8,6 +9,9 @@ from posts.forms import PostForm
 
 # Create your views here.
 def post_list(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+
 	queryset_list = Post.objects.all().order_by("-timestamp")
 	paginator = Paginator(queryset_list,25)
 
@@ -50,6 +54,8 @@ def post_create(request):
 def post_detail(request, id=None):
 	
 	instance = get_object_or_404(Post,id=id)
+	share_string = quote_plus(instance.content)
+
 	context = {
 		"title": "Detail",
 		"instance": instance,
@@ -58,7 +64,8 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_update(request, id=None):
-
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post,id=id)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
@@ -76,6 +83,8 @@ def post_update(request, id=None):
 	return render(request, "post_form.html", context)
 
 def post_delete(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post,id=id)
 	instance.delete()
 	messages.success(request, "Successfully Delete!")
