db01a5c58f8c5cedf330f7cba3f66039b37eaad2
Chris Huey <bchuey415@gmail.com>
2017-04-17 15:09:13 -0700

    reset db; add slugs to post

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
+
 
 # Create your models here.
 
@@ -11,6 +15,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	slug = models.SlugField(unique=True)
 	image = models.ImageField(upload_to=upload_location, 
 		null=True, 
 		blank=True, 
@@ -38,3 +43,27 @@ class Post(models.Model):
 
 	# class Meta:
 		# order = ["-timestamp"]
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug = new_slug
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" %(slug, qs.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
+
+# runs before .save() is called
+def pre_save_post_signal_receiver(sender, instance, *args, **kwargs):
+
+	# slug = slugify(instance.title)
+	# exists = Post.objects.filter(slug=slug).exists()
+	# if exists:
+	# 	slug = "%s-%s" %(slug, instance.id)
+
+	# instance.slug = slug
+	if not instance.slug:
+		instance.slug = create_slug(instance)
+pre_save.connect(pre_save_post_signal_receiver, sender=Post)
