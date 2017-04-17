2060fe3ae9c16b4a1c32df07a4f48d289cb21e60
Chris Huey <bchuey415@gmail.com>
2017-04-17 11:26:06 -0700

    dynamic url routing - detail

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
 	url(r'^$', views.post_list, name='all'),
     url(r'^create/$', views.post_create, name='create'),
-    url(r'^detail/$', views.post_detail, name='detail'),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
     url(r'^update/$', views.post_update, name='update'),
     url(r'^delete/$', views.post_delete, name='delete'),
     # url(r'^posts/$', "<appname>.views.<function_name>"),
--- src/posts/views.py
+++ src/posts/views.py
@@ -26,9 +26,9 @@ def post_create(request):
 
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
+def post_detail(request, id=None):
 	
-	instance = get_object_or_404(Post,id=1)
+	instance = get_object_or_404(Post,id=id)
 	context = {
 		"title": "Detail",
 		"instance": instance,
